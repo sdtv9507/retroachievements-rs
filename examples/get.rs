487822804efce849d5e95c00c37f7181728954d8
sdtv9507 <apr8950@gmail.com>
2022-02-15 07:27:00 -0500

    Update methods

--- examples/get.rs
+++ examples/get.rs
@@ -3,11 +3,11 @@ use retroachievements_rs::RetroAPI;
 fn main() {
     let user = String::from("youruser");
     let key = String::from("yourapikey");
-    let retroa = RetroAPI::new(user, key);
-    let mut response = retroa.request_top_users();
+    let retro = RetroAPI::new(user, key);
+    let mut response = retro.request_top_users();
     println!("Top Users");
     println!("{}", response);
+    response = retro.request_game(11246);
     println!("PSX Final Fantasy Tactics game");
-    response = retroa.request_game(11246);
     println!("{}", response);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -44,4 +44,67 @@ impl RetroAPI {
         let response = self.get_request_with_param(url, game);
         return response;
     }
+
+    pub fn request_game_extend(&self, game_id: i32) -> String {
+        let url = String::from("API_GetGameExtended.php");
+        let game = format!("&i={}", game_id);
+        let response = self.get_request_with_param(url, game);
+        return response;
+    }
+
+    pub fn request_console_id(&self) -> String {
+        let url = String::from("API_GetConsoleIDs.php");
+        let response = self.get_request(url);
+        return response;
+    }
+
+    pub fn request_game_list(&self, console_id: i32) -> String {
+        let url = String::from("API_GetGameList.php");
+        let console = format!("&i={}", console_id);
+        let response = self.get_request_with_param(url, console);
+        return response;
+    }
+
+    pub fn request_feed(&self, user: String, count: i32, offset: i32) -> String {
+        let url = String::from("API_GetFeed.php");
+        let feed = format!("&u={}&c={}&o={}", user, count, offset);
+        let response = self.get_request_with_param(url, feed);
+        return response;
+    }
+
+    pub fn request_user_rank_score(&self, user: String) -> String {
+        let url = String::from("API_GetUserRankAndScore.php");
+        let user = format!("&u={}", user);
+        let response = self.get_request_with_param(url, user);
+        return response;
+    }
+
+    pub fn request_recently_played(&self, user: String, count: i32, offset: i32) -> String {
+        let url = String::from("API_GetUserRecentlyPlayedGames.php");
+        let param = format!("&u={}&c={}&o={}", user, count, offset);
+        let response = self.get_request_with_param(url, param);
+        return response;
+    }
+
+    pub fn request_summary(&self, user: String, games: i32) -> String {
+        let url = String::from("API_GetUserSummary.php");
+        let param = format!("&u={}&g={}&a=5", user, games);
+        let response = self.get_request_with_param(url, param);
+        return response;
+    }
+
+    pub fn request_game_info_progress(&self, user: String, game: i32) -> String {
+        let url = String::from("API_GetGameInfoAndUserProgress.php");
+        let param = format!("&u={}&g={}", user, game);
+        let response = self.get_request_with_param(url, param);
+        return response;
+    }
+
+    pub fn request_games_completed(&self, user: String) -> String {
+        let url = String::from("API_GetUserCompletedGames.php");
+        let param = format!("&u={}", user);
+        let response = self.get_request_with_param(url, param);
+        return response;
+    }
+
 }
