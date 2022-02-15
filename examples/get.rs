9d7b5cefec25749467f410eba163fe36d0acd762
sdtv9507 <apr8950@gmail.com>
2022-02-14 20:09:20 -0500

    Add method to get game

--- examples/get.rs
+++ examples/get.rs
@@ -1,9 +1,13 @@
 use retroachievements_rs::RetroAPI;
 
 fn main() {
-    let user = String::from("yourname");
-    let key = String::from("yourapi");
+    let user = String::from("youruser");
+    let key = String::from("yourapikey");
     let retroa = RetroAPI::new(user, key);
-    let response = retroa.request_top_users();
+    let mut response = retroa.request_top_users();
+    println!("Top Users");
+    println!("{}", response);
+    println!("PSX Final Fantasy Tactics game");
+    response = retroa.request_game(11246);
     println!("{}", response);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,18 @@ impl RetroAPI {
     }
 
     #[tokio::main]
-    pub async fn get_request(&self, url: String) -> String {
+    async fn get_request(&self, url: String) -> String {
         let url = format!("{}{}?z={}&y={}", self.base, url, self.user, self.key);
+        let result = reqwest::get(url).await.unwrap();
+        return result.text().await.unwrap();
+    }
+
+    #[tokio::main]
+    async fn get_request_with_param(&self, url: String, param: String) -> String {
+        let url = format!(
+            "{}{}?z={}&y={}{}",
+            self.base, url, self.user, self.key, param
+        );
         let result = reqwest::get(url).await.unwrap();
         return result.text().await.unwrap();
     }
@@ -25,6 +35,13 @@ impl RetroAPI {
     pub fn request_top_users(&self) -> String {
         let url = String::from("API_GetTopTenUsers.php");
         let response = self.get_request(url);
+        return response;
+    }
+
+    pub fn request_game(&self, game_id: i32) -> String {
+        let url = String::from("API_GetGame.php");
+        let game = format!("&i={}", game_id);
+        let response = self.get_request_with_param(url, game);
         return response;
     }
 }
